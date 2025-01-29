1e0a1ddbf8df84bd48eacb07f27957442bfe5b30
xu3cl40122 <xu3cl40122@gmail.com>
2025-01-29 20:11:29 +0800

    can get file

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"encoding/csv"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/launcher"
@@ -11,59 +13,87 @@ import (
 
 func main() {
 	url := "https://goodinfo.tw/tw2/StockList.asp?RPT_TIME=&MARKET_CAT=%E7%86%B1%E9%96%80%E6%8E%92%E8%A1%8C&INDUSTRY_CAT=%E6%88%90%E4%BA%A4%E9%87%91%E9%A1%8D+%28%E9%AB%98%E2%86%92%E4%BD%8E%29%40%40%E6%88%90%E4%BA%A4%E9%87%91%E9%A1%8D%40%40%E7%94%B1%E9%AB%98%E2%86%92%E4%BD%8E" // 目標網址
+	browser := CreateBrowser()
+	defer browser.MustClose()
+
+	optionList := []string{"1", "301", "601"}
+	fileContentList := DownloadFileByOptions(browser, url, optionList)
+	log.Println("Downloaded", len(fileContentList), "files")
+	aggregateCsvData(fileContentList, "./output/output.csv")
+
+}
+
+func CreateBrowser() *rod.Browser {
+	uBlockPath := "./uBlock"
 	// 設定 Chrome 下載行為，設置下載路徑
 	launcher := launcher.New().
+		// 加載擴充功能
+		Set("load-extension", uBlockPath).
+		// 允許安裝擴充功能
+		Set("extensions-on-chrome-urls").
 		Headless(false)
 
 	browser := rod.New().ControlURL(launcher.MustLaunch()).MustConnect()
-	defer browser.MustClose()
+	return browser
+}
 
-	// 開啟目標網頁
-	page := browser.MustPage(url) // 這裡換成你的目標網址
+func DownloadFileByOptions(browser *rod.Browser, url string, optionList []string) [][]byte {
+	page := browser.MustPage(url)
 	page.MustWaitLoad()
-	page.MustWaitDOMStable()
-	page.MustHandleDialog()
+	output := make([][]byte, 0)
 
-	// page.MustWaitStable()
+	// 對每個選項進行下載
+	for _, option := range optionList {
+		selectElement := page.MustElement(`#selRANK`)
+		selectElement.MustSelect(option)
+		page.MustWaitStable()
 
-	// 找到 select 元素並選擇第二個 option
-	selectElement := page.MustElement(`#selRANK`)
-	log.Println("selectElement:", selectElement)
-	// 等待 select 元素的 child 元素 (所有 option 元素)
-	options := selectElement.MustElements("option")
+		wait := browser.MustWaitDownload()
+		button := page.MustElement(`input[value="匯出CSV"]`)
+		button.MustClick()
 
-	selectElement.MustClick()
-	for i, option := range options {
-		log.Println("option:", i, option.MustText())
-		if i == 1 {
-			option.MustClick()
+		fileContent := wait()
+		output = append(output, fileContent)
+	}
+
+	return output
+}
+
+func aggregateCsvData(fileContentList [][]byte, outputPath string) ([]byte, error) {
+	var allRecords [][]string
+
+	for i, fileContent := range fileContentList {
+		r := bytes.NewReader(fileContent)
+		csvReader := csv.NewReader(r)
+		csvReader.LazyQuotes = true
+
+		records, err := csvReader.ReadAll()
+		if err != nil {
+			return nil, err
+		}
+
+		// 保留第一份的表頭
+		if i == 0 {
+			allRecords = append(allRecords, records...)
+		} else {
+			allRecords = append(allRecords, records[1:]...) // 跳過表頭
 		}
 	}
-	selectElement.MustSelect(`option[value="1"]`)
-	log.Println("選擇第二個 option")
-	page.MustWaitStable()
 
-	// 找到按鈕並點擊
-	wait := browser.MustWaitDownload()
-	button := page.MustElement(`input[value="匯出CSV"]`)
-	button.MustClick()
-	// 等待下載完成並獲取檔案路徑
-	fileContent := wait()
-	println("下載完成:", fileContent)
-	r := bytes.NewReader(fileContent)
-	csvReader := csv.NewReader(r)
-	csvReader.LazyQuotes = true
-
-	records, err := csvReader.ReadAll()
+	// 將合併後的資料寫回 CSV 格式
+	var output strings.Builder
+	csvWriter := csv.NewWriter(&output)
+	err := csvWriter.WriteAll(allRecords)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+	csvWriter.Flush()
+
+	// 將合併後的內容寫入指定路徑的檔案
+	err = os.WriteFile(outputPath, []byte(output.String()), 0644)
+	if err != nil {
+		return nil, err
 	}
 
-	// 逐行處理
-	for i, record := range records {
-		if i <= 3 {
-			log.Println(record)
-		}
-	}
-
+	return []byte(output.String()), nil
 }
