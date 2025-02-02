300183df0703285ab900906afcf87e9c3bd398dd
xu3cl40122 <xu3cl40122@gmail.com>
2025-02-02 10:38:47 +0800

    can export excel

--- main.go
+++ main.go
@@ -1,137 +1,208 @@
 package main
 
 import (
-    "bytes"
-    "encoding/csv"
-    "log"
-    "sync"
-    "github.com/go-rod/rod"
-    "github.com/go-rod/rod/lib/launcher"
-    "github.com/xuri/excelize/v2"
+	"bytes"
+	"context"
+	"encoding/csv"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"sync"
+	"time"
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
+	"github.com/xuri/excelize/v2"
 )
 
+type Config struct {
+	URL             string              `json:"url"`
+	CategoryMap     map[string][]string `json:"categoryMap"`
+	Headless        bool                `json:"headless"`
+	OutputPath      string              `json:"outputPath"`
+	CountOptionList []string            `json:"countOptionList"`
+	ConcurrentCount int            `json:"concurrentCount"`
+}
+
 func main() {
-    url := "https://goodinfo.tw/tw2/StockList.asp?RPT_TIME=&MARKET_CAT=%E7%86%B1%E9%96%80%E6%8E%92%E8%A1%8C&INDUSTRY_CAT=%E6%88%90%E4%BA%A4%E9%87%91%E9%A1%8D+%28%E9%AB%98%E2%86%92%E4%BD%8E%29%40%40%E6%88%90%E4%BA%A4%E9%87%91%E9%A1%8D%40%40%E7%94%B1%E9%AB%98%E2%86%92%E4%BD%8E"
-    categoryMap := map[string][]string{
-        "成交額": {"交易狀況–成交資料"},
-        "短漲跌": {"交易狀況–漲跌及成交統計", "短期累計漲跌統計"},
-        // 其他檔名和 URL
-    }
+	// 讀取 JSON 設定檔
+	log.Println("=== Start ===")
+	startTime := time.Now()
+	config := GetConfig("./config.json")
+	var wg sync.WaitGroup
+	var mu sync.Mutex
 
-    countOptionList := []string{"1", "301", "601"}
-    outputDir := "./output"
-    var wg sync.WaitGroup
+	f := excelize.NewFile()
 
-    f := excelize.NewFile()
+	// 創建一個帶緩衝的 channel 作為信號量，限制同時運行的 goroutine 數量
+	sem := make(chan struct{}, config.ConcurrentCount)
 
-    for fileName, categoryList := range categoryMap {
-        wg.Add(1)
-        go func(fileName, url string) {
-            defer wg.Done()
-            GetDataFlow(fileName, url, categoryList, countOptionList, outputDir, f)
-        }(fileName, url)
-    }
+	for tabName, categoryList := range config.CategoryMap {
+		wg.Add(1)
+		go func(fileName, url string) {
+			defer wg.Done()
+			sem <- struct{}{}        // 獲取信號量
+			defer func() { <-sem }() // 釋放信號量
 
-    wg.Wait()
+			startTime := time.Now()
+			log.Printf("Processing %s", fileName)
+			err := processURL(fileName, config, categoryList, f)
+			if err != nil {
+				log.Printf("Error processing %s: %v", fileName, err)
+				return
+			}
+			elapsedTime := time.Since(startTime).Round(time.Second)
+			log.Printf("Finished %s took %s", fileName, elapsedTime)
+		}(tabName, config.URL)
+	}
 
-    // 保存 Excel 文件
-    if err := f.SaveAs(outputDir + "/output.xlsx"); err != nil {
-        log.Fatal(err)
-    }
+	wg.Wait()
+
+	// 獲取當前時間並格式化
+	formattedTime := time.Now().Format("2006_01_02")
+
+	// 保存 Excel 文件
+	outputFilePath := fmt.Sprintf("%s/output_%s.xlsx", config.OutputPath, formattedTime)
+	mu.Lock()
+	if err := f.SaveAs(outputFilePath); err != nil {
+		log.Fatal(err)
+	}
+	mu.Unlock()
+	log.Println("=== Finished ===", "Time:", time.Since(startTime).Round(time.Second))
 }
 
-func GetDataFlow(fileName, url string, categoryList, countOptionList []string, outputDir string, f *excelize.File) {
-    browser := CreateBrowser()
-    defer browser.MustClose()
+func GetConfig(path string) Config {
+	configFile, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer configFile.Close()
 
-    fileContentList := DownloadFileByOptions(browser, url, categoryList, countOptionList)
+	byteValue, err := io.ReadAll(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    err := aggregateCsvDataToExcel(fileContentList, f, fileName)
-    if err != nil {
-        log.Fatal(err)
-    }
+	var config Config
+	if err := json.Unmarshal(byteValue, &config); err != nil {
+		log.Fatal(err)
+	}
+	return config
 }
 
-func CreateBrowser() *rod.Browser {
-    uBlockPath := "./uBlock"
-    launcher := launcher.New().
-        Set("load-extension", uBlockPath).
-        Set("extensions-on-chrome-urls").
-        Headless(false)
+func processURL(fileName string, config Config, categoryList []string, f *excelize.File) error {
+	browser := CreateBrowser(config.Headless)
+	defer browser.MustClose()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
 
-    browser := rod.New().ControlURL(launcher.MustLaunch()).MustConnect()
-    return browser
+	fileContentList, err := DownloadFileByOptions(ctx, browser, config.URL, categoryList, config.CountOptionList)
+	if err != nil {
+		return fmt.Errorf("error downloading files for %s: %w", fileName, err)
+	}
+
+	err = aggregateCsvDataToExcel(fileContentList, f, fileName)
+	if err != nil {
+		return fmt.Errorf("error aggregating CSV data for %s: %w", fileName, err)
+	}
+
+	return nil
+}
+
+func CreateBrowser(headless bool) *rod.Browser {
+	uBlockPath := "./uBlock"
+	launcher := launcher.New().
+		Set("load-extension", uBlockPath).
+		Set("extensions-on-chrome-urls")
+
+	if headless {
+		launcher = launcher.Headless(true)
+	} else {
+		launcher = launcher.Headless(false)
+	}
+
+	browser := rod.New().ControlURL(launcher.MustLaunch()).MustConnect()
+	return browser
 }
 
 func getSelectorIdByIndex(i int) string {
-    switch i {
-    case 0:
-        return "#selSHEET"
-    case 1:
-        return "#selSHEET2"
-    }
-    return ""
+	switch i {
+	case 0:
+		return "#selSHEET"
+	case 1:
+		return "#selSHEET2"
+	}
+	return ""
 }
 
-func DownloadFileByOptions(browser *rod.Browser, url string, categoryList []string, countOptionList []string) [][]byte {
-    page := browser.MustPage(url)
-    page.MustWaitLoad()
-    output := make([][]byte, 0)
+func DownloadFileByOptions(ctx context.Context, browser *rod.Browser, url string, categoryList []string, countOptionList []string) ([][]byte, error) {
+	page := browser.MustPage(url)
+	page.MustWaitLoad()
+	output := make([][]byte, 0)
 
-    for _, option := range countOptionList {
-        for i, category := range categoryList {
-            selectNodeOfCategory := page.MustElement(getSelectorIdByIndex(i))
-            selectNodeOfCategory.MustSelect(category)
-            page.MustWaitStable()
-        }
-        selectNodeOfCount := page.MustElement(`#selRANK`)
-        selectNodeOfCount.MustSelect(option)
-        page.MustWaitStable()
+	for _, option := range countOptionList {
+		for i, category := range categoryList {
+			selectNodeOfCategory := page.MustElement(getSelectorIdByIndex(i))
+			selectNodeOfCategory.MustSelect(category)
+			page.MustWaitStable()
+		}
+		selectNodeOfCount := page.MustElement(`#selRANK`)
+		selectNodeOfCount.MustSelect(option)
+		page.MustWaitStable()
 
-        wait := browser.MustWaitDownload()
-        button := page.MustElement(`input[value="匯出CSV"]`)
-        button.MustClick()
+		wait := browser.MustWaitDownload()
+		button := page.MustElement(`input[value="匯出CSV"]`)
+		button.MustClick()
 
-        fileContent := wait()
-        output = append(output, fileContent)
-    }
+		fileContent := wait()
+		output = append(output, fileContent)
 
-    return output
+		select {
+		case <-ctx.Done():
+			return nil, errors.New("Timeout")
+		default:
+
+		}
+	}
+
+	return output, nil
 }
 
 func aggregateCsvDataToExcel(fileContentList [][]byte, f *excelize.File, sheetName string) error {
-    var allRecords [][]string
+	var allRecords [][]string
 
-    for i, fileContent := range fileContentList {
-        r := bytes.NewReader(fileContent)
-        csvReader := csv.NewReader(r)
-        csvReader.LazyQuotes = true
+	for i, fileContent := range fileContentList {
+		r := bytes.NewReader(fileContent)
+		csvReader := csv.NewReader(r)
+		csvReader.LazyQuotes = true
 
-        records, err := csvReader.ReadAll()
-        if err != nil {
-            return err
-        }
+		records, err := csvReader.ReadAll()
+		if err != nil {
+			return err
+		}
 
-        if i == 0 {
-            allRecords = append(allRecords, records...)
-        } else {
-            allRecords = append(allRecords, records[1:]...)
-        }
-    }
+		if i == 0 {
+			allRecords = append(allRecords, records...)
+		} else {
+			allRecords = append(allRecords, records[1:]...)
+		}
+	}
 
-    // 創建新的工作表
-    index, _ := f.NewSheet(sheetName)
+	// 創建新的工作表
+	index, _ := f.NewSheet(sheetName)
 
-    // 將合併後的資料寫入工作表
-    for i, record := range allRecords {
-        for j, cell := range record {
-            cellName, _ := excelize.CoordinatesToCellName(j+1, i+1)
-            f.SetCellValue(sheetName, cellName, cell)
-        }
-    }
+	// 將合併後的資料寫入工作表
+	for i, record := range allRecords {
+		for j, cell := range record {
+			cellName, _ := excelize.CoordinatesToCellName(j+1, i+1)
+			f.SetCellValue(sheetName, cellName, cell)
+		}
+	}
 
-    // 設置活動工作表
-    f.SetActiveSheet(index)
+	// 設置活動工作表
+	f.SetActiveSheet(index)
 
-    return nil
+	return nil
 }
